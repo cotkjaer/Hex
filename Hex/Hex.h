934cb36fafa949182e61d7c42efe47031e932b73
christian otkjær <christian.otkjaer@gmail.com>
2016-05-20 14:00:51 +0200

    Initial Commit

---
+++ Hex/Hex.h
@@ -0,0 +1,19 @@
+//
+//  Hex.h
+//  Hex
+//
+//  Created by Christian Otkjær on 20/05/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hex.
+FOUNDATION_EXPORT double HexVersionNumber;
+
+//! Project version string for Hex.
+FOUNDATION_EXPORT const unsigned char HexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hex/PublicHeader.h>
+
+
